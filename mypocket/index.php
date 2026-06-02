7cf792ec913391992bce83d441aa9c3848c000ca
wagnermarques <wagnerdocri@gmail.com>
2026-06-02 09:05:50 -0300

    Falta front

--- mypocket/index.php
+++ mypocket/index.php
@@ -3,3 +3,19 @@
 
 
 ?>
+
+<html>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>MyPocket</title>
+    </head>
+    <body>
+        <form action="processa.php" method="POST">
+            <input type="text">
+        </form>
+    </body>
+    </html>
+</html>
--- mypocket/processa.php
+++ mypocket/processa.php
@@ -1,5 +1,12 @@
 <?php
+declare(strict_types=1);
 
+session_start();
 
+require_once "classes/Carteira.php";
+
+if(isset($_SESSION['carteira'])){
+    
+}
 
 ?>
