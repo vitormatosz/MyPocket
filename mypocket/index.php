8c2163dc5563e3fc137ae6ddc67f1ebff28d37e9
unknown <vitorqsmatos@gmail.com>
2026-06-08 11:06:56 -0300

    P

--- mypocket/index.php
+++ mypocket/index.php
@@ -33,20 +33,20 @@ $carteira = $_SESSION['carteira'];
             <div class="box">
                 <h2 class="subtitle">Saldo Atual</h2>
 
-                <p class="title has-text-link">
+                <p class="title has-text-info-35">
                     R$ <?= number_format($carteira->getSaldo(), 2, ',', '.') ?>
                 </p>
             </div>
 
-            <div class="box mt-6">
+            <div class="box mt-6 has-background-info-35 ">
 
-                <h3 class="title is-3">
+                <h3 class="title is-3 has-text-primary-15-invert">
                     Nova Transação
                 </h3>
 
                 <form action="processa.php" method="POST">
                     <div class="field">
-                        <label class="label">Valor</label>
+                        <label class="label has-text-primary-15-invert">Valor</label>
 
                         <div class="control">
                             <input class="input" step="0.01" type="number" name="valor" required>
@@ -54,7 +54,7 @@ $carteira = $_SESSION['carteira'];
                     </div>
 
                     <div class="field">
-                        <label class="label">Tipo</label>
+                        <label class="label has-text-primary-15-invert">Tipo</label>
 
                         <div class="control">
                             <div class="select">
@@ -67,13 +67,13 @@ $carteira = $_SESSION['carteira'];
                     </div>
 
                     <div class="field">
-                        <label class="label">Descrição</label>
+                        <label class="label has-text-primary-15-invert">Descrição</label>
                         <div class="control"><input class="input" type="text" name="descricao" required>
                         </div>
                     </div>
 
                     <div class="field">
-                        <label class="label">Data</label>
+                        <label class="label has-text-primary-15-invert">Data</label>
                         <div class="control">
                             <input class="input" type="date" name="data" required>
                         </div>
@@ -81,7 +81,7 @@ $carteira = $_SESSION['carteira'];
 
                     <div class="field ">
                         <div class="control">
-                            <button class="button is-link" style="width: 200px;">Adicionar</button>
+                            <button class="button is-link has-background-link-100 has-text-primary-85-invert" style="width: 200px;">Adicionar</button>
                         </div>
                     </div>
 
@@ -94,10 +94,10 @@ $carteira = $_SESSION['carteira'];
 
                 <table class="table is-striped is-hoverable is-fullwidth">
                     <tr>
-                        <th class="title is-4">Valor</th>
-                        <th class="title is-4">Tipo</th>
-                        <th class="title is-4">Descrição</th>
-                        <th class="title is-4">Data</th>
+                        <th class="title is-4 has-text-info-35">Valor</th>
+                        <th class="title is-4 has-text-info-35">Tipo</th>
+                        <th class="title is-4 has-text-info-35">Descrição</th>
+                        <th class="title is-4 has-text-info-35">Data</th>
                     </tr>
 
                     <tr>
